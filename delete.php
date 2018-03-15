bd05b3e9cfba75cc124ecea2979a6e820098ce16
dpb246 <dpb246@users.noreply.github.com>
2018-03-15 14:35:32 -0400

    Require login to view active users

--- delete.php
+++ delete.php
@@ -1,10 +1,12 @@
 <?php
-	session_start();
-	$u = $_SESSION['userid'];
-	$id = $_POST['id'];
+	if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
 	require('db.php');
 	require('isadmin.php');
-	
+	$u = $_SESSION['userid'];
+	$id = $_POST['id'];
+		
 	$stmt = $pdo->prepare('DELETE FROM messages WHERE id=?');
 	$stmt->execute([$id]);
 	
--- viewusers.php
+++ viewusers.php
@@ -1,4 +1,9 @@
 <?php
+	if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
+	require("ipblock.php");
+	require("check_login.php");
 	require("db.php");
 	$stmt = $pdo->prepare("SELECT * FROM users WHERE lastactive >= CURRENT_TIMESTAMP - interval '4' minute;");
 	$stmt->execute([]);
